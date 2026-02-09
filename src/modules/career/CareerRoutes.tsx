f26301fcc6a28c18364b562518252170678d5615
Moeed Ahmed <moeedahmed@users.noreply.github.com>
2026-02-09 18:12:44 +0000

    fix: wrap career routes with GoalWizard + PathwayWizard providers

--- src/modules/career/CareerRoutes.tsx
+++ src/modules/career/CareerRoutes.tsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { GoalWizardProvider } from '@/modules/career/contexts/GoalWizardContext';
+import { PathwayWizardProvider } from '@/modules/career/contexts/PathwayWizardContext';
 import DashboardPage from './pages/DashboardPage';
 import GoalsPage from './pages/GoalsPage';
 import PathwaysPage from './pages/PathwaysPage';
@@ -6,11 +9,17 @@ import RoadmapPage from './pages/RoadmapPage';
 
 export function CareerRoutes() {
   return (
-    <Routes>
-      <Route index element={<DashboardPage />} />
-      <Route path="goals" element={<GoalsPage />} />
-      <Route path="pathways" element={<PathwaysPage />} />
-      <Route path="roadmap/:id" element={<RoadmapPage />} />
-    </Routes>
+    <TooltipProvider>
+      <GoalWizardProvider>
+        <PathwayWizardProvider>
+          <Routes>
+            <Route index element={<DashboardPage />} />
+            <Route path="goals" element={<GoalsPage />} />
+            <Route path="pathways" element={<PathwaysPage />} />
+            <Route path="roadmap/:id" element={<RoadmapPage />} />
+          </Routes>
+        </PathwayWizardProvider>
+      </GoalWizardProvider>
+    </TooltipProvider>
   );
 }
